daf99058ef4381197d527ead143c33a27ec6c23b
hoang <minhhoangho99@gmail.com>
2020-11-10 15:42:41 +0700

    update knex file

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ Env.config();
 
 const BASE_DB_PATH = path.join(__dirname, 'src', 'database');
 
-export default {
+module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL_DEV,
